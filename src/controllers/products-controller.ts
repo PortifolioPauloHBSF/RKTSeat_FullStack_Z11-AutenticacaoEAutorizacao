ee32530c43484518b691a41125ffa44029b4c29f
PauloHBSF <paulohbsfilho@gmail.com>
2025-06-14 16:37:20 -0300

    :sparkles: extraindo id do usuário do token informado no auth header

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -6,7 +6,7 @@ class ProductsController {
     }
 
     async create(request: Request, response: Response) {
-        return response.status(201).json({ message: "Created" });
+        return response.status(201).json({ message: request.user?.id });
     }
 }
 
---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,22 @@
+import { AppError } from "@/utils/AppError";
+import { Request, Response, NextFunction } from "express";
+import { verify } from "jsonwebtoken";
+import { authConfig } from "@/configs/auth";
+
+function ensureAuthenticated(request: Request, response: Response, next: NextFunction) {
+    const authHeader = request.headers.authorization;
+
+    if (!authHeader) {
+        throw new AppError("Cannont find JWT", 401);
+    }
+
+    const [_, token] = authHeader.split(" ");
+    const { sub: user_id } = verify(token, authConfig.jwt.secret);
+    request.user = {
+        id: String(user_id),
+    };
+
+    return next();
+}
+
+export { ensureAuthenticated };
