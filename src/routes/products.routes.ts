bfc3fcbc4a667ab2a6e985c3493fb40b92c7edcc
PauloHBSF <paulohbsfilho@gmail.com>
2025-06-14 17:13:31 -0300

    :sparkles: permitindo apenas roles específicas

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { ProductsController } from "@/controllers/products-controller";
 import { ensureAuthenticated } from "@/middlewares/ensureAuthenticated";
+import { verifyUserAuthorization } from "@/middlewares/veifyUserAuthorization";
 
 const productsRoutes = Router();
 const productsController = new ProductsController();
 
 productsRoutes.get("/", productsController.index);
-productsRoutes.post("/", ensureAuthenticated, productsController.create);
+productsRoutes.post("/", ensureAuthenticated, verifyUserAuthorization(['sale', 'admin']), productsController.create);
 
 export { productsRoutes };
